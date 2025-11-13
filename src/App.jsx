c406da4c6ae6907331d1529b5d14f182d883de61
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-11-13 12:48:18 +0100

    created author and title field

--- src/App.jsx
+++ src/App.jsx
@@ -13,24 +13,47 @@ function App() {
     public: false
   }
 
+   const [formData, setFormData] = useState({
+    name: '',
+    language: '',
+    urgency: 'low',
+    updates: false,
+    description: ''
+
+  })
+
   return (
     <>
       <h1>Scrivi il tuo articolo</h1>
       <form>
   <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <label  className="form-label">Autore</label>
+    <input 
+    type="text" 
+    className="form-control" 
+    name='author'
+    value={FormData.author}
+    required
+    onChange={(e) => setFormData({...formData, name: e.target.value})}
+    />
+  </div>
+  <div className="mb-3">
+    <label className="form-label">Titolo dell'articolo</label>
+    <input type="text"
+    className="form-control"
+    name='title'
+    onChange={(e) => setFormData({...formData, name:e.target.value})}
+     />
   </div>
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1"/>
+    <label  className="form-label">Scrivi qui il tuo articolo</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
   </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  <div className="mb-3 form-check">
+    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+    <label className="form-check-label" for="exampleCheck1">Check me out</label>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" className="btn btn-primary">Submit</button>
   </form>
 
     </>
