2c7dda08246837315ea0cf4849f13b9294f0af98
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-11-13 17:57:08 +0100

    now it works

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
    const [formData, setFormData] = useState(InitialFormData)
 
-   function handleSubmit(e) {
+   /*function handleSubmit(e) {
     e.preventdefault()
     console.log(formData);
 
@@ -26,60 +26,70 @@ function App() {
     console.log(response);
     setFormData(InitialFormData)
    })
-  }
+  }*/
+ function handleSubmit(e){
+  e.preventDefault();
+  console.log('cioa')
+   axios.post(endpoint, formData, {
+      headers: {'content-type' : 'application/json'}
+    }).then((res)=>{
+      console.log(res)
+      setFormData(InitialFormData)
+    })
+ }
 
   return (
     <>
       <h1>Scrivi il tuo articolo</h1>
       <form onSubmit={handleSubmit}>
-  <div class="mb-3">
-    <label  className="form-label">Autore</label>
-    <input 
-    type="text" 
-    className="form-control" 
-    name='author'
-    value={FormData.author}
-    required
-    onChange={(e) => setFormData({...formData, name: e.target.value})}
-    />
-  </div>
-  <div class="mb-3">
-    <label  className="form-label">Titolo</label>
-    <input 
-    type="text" 
-    className="form-control" 
-    name='title'
-    value={FormData.title}
-    required
-    onChange={(e) => setFormData({...formData, name: e.target.value})}
-    />
-  </div>
-  <div class="mb-3">
-    <label  className="form-label">Scrivi qui il tuo articolo</label>
-    <input 
-    type="text" 
-    className="form-control" 
-    name='body'
-    value={FormData.body}
-    required
-    onChange={(e) => setFormData({...formData, name: e.target.value})}
-     />
-  </div>
-  <div className="mb-3 form-check">
-    <label  className="form-label">Spunta se vuoi renderlo pubblico, sennò rimarrà come bozza privata</label>
-    <input
-    type='checkbox'
-    name='public'
-    className="" 
-    value={formData.public}
-    onChange={(e) => setFormData({...formData, name: e.target.value})}
-    >
-    
-    </input>
+        <div className="mb-3">
+          <label  className="form-label">Autore</label>
+          <input 
+          type="text" 
+          className="form-control" 
+          name='author'
+          value={FormData.author}
+          required
+          onChange={(e) => setFormData({...formData, name: e.target.value})}
+          />
+        </div>
+        <div className="mb-3">
+          <label  className="form-label">Titolo</label>
+          <input 
+          type="text" 
+          className="form-control" 
+          name='title'
+          value={FormData.title}
+          required
+          onChange={(e) => setFormData({...formData, name: e.target.value})}
+          />
+        </div>
+        <div className="mb-3">
+          <label  className="form-label">Scrivi qui il tuo articolo</label>
+          <input 
+          type="text" 
+          className="form-control" 
+          name='body'
+          value={FormData.body}
+          required
+          onChange={(e) => setFormData({...formData, name: e.target.value})}
+          />
+        </div>
+        <div className="mb-3 form-check">
+          <label  className="form-label">Spunta se vuoi renderlo pubblico, sennò rimarrà come bozza privata</label>
+          <input
+          type='checkbox'
+          name='public'
+          className="" 
+          value={formData.public}
+          onChange={(e) => setFormData({...formData, name: e.target.value})}
+          >
+          
+          </input>
 
-  </div>
-  <button type="submit" className="btn btn-primary"  >Submit</button>
-  </form>
+        </div>
+        <button type="submit" className="btn btn-primary"  >Submit</button>
+      </form>
 
     </>
   )
